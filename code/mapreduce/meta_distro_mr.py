da89f404a562ef91b785898b7ea83948de5e42ca
Erin M. Ochoa <emo@uchicago.edu>
2017-05-10 17:12:33 -0500

    Added script to find the 10 most eclectic songs (based on lowest mean dc measures)

--- code/mapreduce/meta_distro_mr.py
+++ code/mapreduce/meta_distro_mr.py
@@ -1,8 +1,6 @@
 import numpy as np
 from mrjob.job import MRJob
-from scipy.stats import skew as skew
 
-#9552	[0.5286862605730831, 0.9920071518476695, 0.8119728761566468, 0.798583681936698, 0.09800582444148233, -0.31780970738749975]
 
 class MetaDistribution(MRJob):
     '''
@@ -16,7 +14,7 @@ class MetaDistribution(MRJob):
         distro_str = distro_str.strip('[]').split(',')
 
         dicto = {'dMin':None}
-        dicto['dMAx'] = None
+        dicto['dMax'] = None
         dicto['dMedian'] = None
         dicto['dMean'] = None
         dicto['dSD'] = None
@@ -31,15 +29,11 @@ class MetaDistribution(MRJob):
             yield key, value
 
 
-
-
     def reducer(self, key, value):
         '''
         '''
 
         yield key, np.mean([x for x in value])
-
-
 
 
 if __name__ == '__main__':
---
+++ code/mapreduce/most_eclectic_mr.py
@@ -0,0 +1,53 @@
+import numpy as np
+from mrjob.job import MRJob
+from queue import PriorityQueue
+
+
+class MostEclectic(MRJob):
+    '''
+    This is based on taking distances between songs with distance correlation,
+    which should be bounded between [0,1], with values closer to 0 indicating
+    greater independence and values closer to 1 indicating less independence.
+    '''
+
+    def mapper(self, _, line):
+        '''
+        '''
+        songIDX, distro_str = line.split('\t')
+
+        distro_str = distro_str.strip('[]').split(',')
+
+        mean_dist = float(distro_str[3])
+
+        yield songIDX, mean_dist
+
+
+    def reducer_init(self):
+        self.songqueue = PriorityQueue(10)
+        self.distances = []
+
+
+    def reducer(self, songIDX, mean_dist):
+        '''
+        '''
+
+        dist = np.mean([x for x in mean_dist])
+
+        if len(self.distances) < 10:
+            self.distances.append(-dist)
+            self.songqueue.put(tuple((-dist,songIDX)))
+        elif -dist > min(self.distances) and len(self.distances) == 10:
+            self.distances.remove(min(self.distances))
+            self.distances.append(-dist)
+            _ = self.songqueue.get()
+            self.songqueue.put(tuple((-dist,songIDX)))
+
+
+    def reducer_final(self):
+        while not self.songqueue.empty():
+            distance, songIDX = self.songqueue.get()
+            yield songIDX, -distance
+
+
+if __name__ == '__main__':
+    MostEclectic.run()
