8168a1b72f97ab6abb2051be2ce62e91b5799064
Erin M. Ochoa <emo@uchicago.edu>
2017-05-10 20:09:58 -0500

    Changed csvs to tsvs; wrote a shell script to run MapReduce jobs

---
+++ code/mapreduce/run_mr_jobs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+# Use the script that passes pairs of filepaths to compute pairwise distances
+#python3 distance1D_mr.py mapreduce.job.reduces=1 pairs_trimmed.csv > temp_distances.tsv
+
+# Characteristics for individual distributions (per song)
+python3 distro_mr.py mapreduce.job.reduces=1 temp_distances.tsv > temp_distributions.tsv
+
+# Characteristics of the overall average distribution
+python3 meta_distro_mr.py mapreduce.job.reduces=1 temp_distributions.tsv > temp_meta_distribution.tsv
+
+# Top 10 most eclectic songs in the dataset
+python3 most_eclectic_mr.py mapreduce.job.reduces=1 temp_distributions.tsv > temp_most_eclectic.tsv
+
+# Top 10 most formulaic songs in the dataset
+python3 most_formulaic_mr.py mapreduce.job.reduces=1 temp_distributions.tsv > temp_most_formulaic.tsv
