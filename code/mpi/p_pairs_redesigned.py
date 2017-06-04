792c45aae84767cef913a08d5dde0825f7219182
Rodrigo Valdes <rodrigovaldes@uchicago.edu>
2017-06-04 08:50:37 +0000

    changes p_pairs better idea

--- code/mpi/p_pairs_redesigned.py
+++ code/mpi/p_pairs_redesigned.py
@@ -168,6 +168,9 @@ def process_pickle_pairs(send_names_files, rank, size):
             list_pickles.append((pickle_to_list_1, pickle_to_list_2))
             print(len(list_pickles))
             print(len(list_pickles[0]))
+            # print(list_pickles[0])
+            print(list_pickles[0][0].keys())
+            print(list_pickles[0][0][0].keys())
     else:
         list_pickles = None
     
