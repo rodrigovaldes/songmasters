0d114a70ea4f60e5107daca72e3200137aad16a7
Erin M. Ochoa <emo@uchicago.edu>
2017-06-05 12:37:45 -0500

    Edit

--- code/mapreduce/run_mr_jobs_DATAPROC.sh
+++ code/mapreduce/run_mr_jobs_DATAPROC.sh
@@ -5,25 +5,25 @@
 
 # Uses the script that unpickles the catalog to compute pairwise distances
 # TESTING on 100K sample of comparisons
-#python3 pickle2distance1D_mr.py --jobconf mapreduce.job.reduces=1 pairsIDX_trimmed.csv > test_distances.tsv
+#python3 pickle2distance1D_mr.py dataproc --num-core-instances 4 pairsIDX_trimmed.csv > test_distances.tsv
 
 # Running the full 10K songs comparisons
-#python3 pickle2distance1D_mr.py --jobconf mapreduce.job.reduces=1 pairsIDX.csv > distances_10ksongs.tsv
+#python3 pickle2distance1D_mr.py dataproc --num-core-instances 4 pairsIDX.csv > distances_10ksongs.tsv
 
 # Characteristics for individual distributions (per song)
-python3 distro_mr.py --jobconf mapreduce.job.reduces=1 distances_10ksongs.tsv > distributions_10ksongs.tsv
+python3 distro_mr.py dataproc --num-core-instances 4 distances_10ksongs.tsv > distributions_10ksongs.tsv
 
 # Characteristics of the overall average distribution
-python3 meta_distro_mr.py --jobconf mapreduce.job.reduces=1 distributions_10ksongs.tsv > meta_distribution_10ksongs.tsv
+python3 meta_distro_mr.py dataproc --num-core-instances 4 distributions_10ksongs.tsv > meta_distribution_10ksongs.tsv
 
 # Top 10 most eclectic songs in the dataset
-python3 most_eclectic_mr.py --jobconf mapreduce.job.reduces=1 distributions_10ksongs.tsv > most_eclectic_10Ksongs.tsv
+python3 most_eclectic_mr.py dataproc --num-core-instances 4 distributions_10ksongs.tsv > most_eclectic_10Ksongs.tsv
 
 # Top 10 most formulaic songs in the dataset
-python3 most_formulaic_mr.py --jobconf mapreduce.job.reduces=1 distributions_10ksongs.tsv > most_formulaic_10ksongs.tsv
+python3 most_formulaic_mr.py dataproc --num-core-instances 4 distributions_10ksongs.tsv > most_formulaic_10ksongs.tsv
 
 # Finds the two most similar songs in the dataset
-python3 most_similar_mr.py --jobconf mapreduce.job.reduces=1 distances_10ksongs.tsv > most_similar_10ksongs.tsv
+python3 most_similar_mr.py dataproc --num-core-instances 4 distances_10ksongs.tsv > most_similar_10ksongs.tsv
 
 # Finds the two most dissimilar songs in the dataset
-python3 least_similar_mr.py --jobconf mapreduce.job.reduces=1 distances_10ksongs.tsv > least_similar_10ksongs.tsv
+python3 least_similar_mr.py dataproc --num-core-instances 4 distances_10ksongs.tsv > least_similar_10ksongs.tsv
