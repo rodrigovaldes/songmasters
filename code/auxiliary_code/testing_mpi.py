d93cf726605f7f1da98600ab3ef15480c8a314ad
rodrigovaldes <rodrigovaldes@uchicago.edu>
2017-06-05 22:36:17 -0500

    testing

---
+++ code/auxiliary_code/testing_mpi.py
@@ -0,0 +1,32 @@
+import os
+import pickle
+import numpy as np
+from mpi4py import MPI
+from queue import Queue
+from scipy.misc import comb
+from operator import itemgetter
+from itertools import combinations as combo
+from sklearn.metrics.pairwise import cosine_similarity as cs
+
+comm = MPI.COMM_WORLD
+rank, size = comm.Get_rank(), comm.Get_size()
+
+print('Rank = {}; size = {}'.format(rank,size))
+
+if rank == 0:
+
+	create_mode = np.arange(size)
+
+else:
+
+    create_mode = None
+
+one_part = comm.scatter(create_mode, root=0) 
+
+one_part += 3
+
+all_this = comm.gather(one, root=0)
+
+if rank == 0:
+	print(all_this)
+
