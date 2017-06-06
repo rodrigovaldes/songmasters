ad6a12329081d298f1d1cad8b8e136d149371313
Erin M. Ochoa <emo@uchicago.edu>
2017-06-06 08:55:19 -0500

    Fixed pick_pickles

--- code/mpi/pick_pickles.py
+++ code/mpi/pick_pickles.py
@@ -1,6 +1,10 @@
 from scipy.misc import comb
 
+
 NODES = 6
+
+UPPER = 100
+LOWER = NODES
 
 def diagnostic(pickles, NODES):
     com = comb(pickles,2)
@@ -11,7 +15,12 @@ def diagnostic(pickles, NODES):
 
 
 
-def pick_pickles(lower, upper, NODES):
-    for i in range(lower, upper + 1):
+def pick_pickles(LOWER, UPPER, NODES):
+    print('The following pickle numbers are appropriate for use with {} nodes:'.format(NODES))
+    for i in range(LOWER, UPPER + 1):
         if diagnostic(i, NODES):
             print(i)
+
+
+if __name__ == '__main__':
+    pick_pickles(LOWER,UPPER,NODES)
