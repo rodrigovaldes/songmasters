0ccb47fccaa70fa1adbff4da658c4039f4659d0e
Erin M. Ochoa <emochoa@users.noreply.github.com>
2017-06-06 15:18:24 -0500

    Reverting to an older version

--- code/mapreduce/most_formulaic_mr.py
+++ code/mapreduce/most_formulaic_mr.py
@@ -4,9 +4,7 @@ from queue import PriorityQueue
 
 class MostFormulaic(MRJob):
     '''
-    This is based on taking distances between songs with distance correlation,
-    which should be bounded between [0,1], with values closer to 0 indicating
-    greater independence and values closer to 1 indicating less independence.
+    This is based on taking distances between songs with cosine similarity.
     '''
 
     def mapper(self, _, line):
@@ -33,13 +31,13 @@ class MostFormulaic(MRJob):
         dist = list(mean_dist)[0]
 
         if len(self.distances) < 10:
-            self.distances.append(abs(dist))
+            self.distances.append(dist)
             self.songqueue.put(tuple((dist,songIDX)))
-        elif abs(dist) > min(self.distances) and len(self.distances) == 10:
+        elif dist > min(self.distances) and len(self.distances) == 10:
             self.distances.remove(min(self.distances))
-            self.distances.append(abs(dist))
+            self.distances.append(dist)
             _ = self.songqueue.get()
-            self.songqueue.put(tuple((abs(dist),songIDX)))
+            self.songqueue.put(tuple((dist,songIDX)))
 
 
     def reducer_final(self):
